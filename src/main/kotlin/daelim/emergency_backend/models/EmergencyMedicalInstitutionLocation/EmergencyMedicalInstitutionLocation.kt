21e789f73d3ea2f5d08400afcc70e23bee49d79e
huibeom <heebum9588@naver.com>
2024-09-26 22:40:14 +0900

    4. 응급의료기관 위치정보 데이터 클래스 정의
    Data Class 정의
    -아래는 지라 링크
    https://daelim-univ.atlassian.net/browse/PRO-273

---
+++ src/main/kotlin/daelim/emergency_backend/models/EmergencyMedicalInstitutionLocation/EmergencyMedicalInstitutionLocation.kt
@@ -0,0 +1,21 @@
+package daelim.emergency_backend.models
+
+
+//응급의료기관 위치정보 조회
+data class EmergencyMedicalInstitutionLocation(
+    val rnum: Int,//일련번호
+    val cnt: Int,//건수
+    val distance: Double,//거리,
+    val dutyAddr: String,//주소
+    val dutyDiv: String, //병원 분류
+    val dutyDivName: String, //병원 분류명
+    val dutyFax: String, //팩스번호
+    val dutyName: String,//기관명
+    val dutyTel1: String,//대표전화1
+    val endTime:String,//종료시간
+    val hpid: String,//기관ID
+    val latitude: Double,//병원위도
+    val longitude: Double,//병원경도
+    val startTime: String,//시작시간
+
+)
---
+++ src/main/kotlin/daelim/emergency_backend/models/EmergencyMedicalInstitutionLocation/EmergencyMedicalInstitutionLocationResult.kt
@@ -0,0 +1,8 @@
+package daelim.emergency_backend.models
+
+data class EmergencyMedicalInstitutionLocationResult(
+val resultCode:Int,//결과코드
+val resultMag : String, //결과메시지
+
+val Items: List<EmergencyMedicalInstitutionLocation>
+)
