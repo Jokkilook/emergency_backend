5f3a409d6f9e1655a0953b45d97ab0fbca46db39
huibeom <heebum9588@naver.com>
2024-09-27 00:09:16 +0900

    re:[ 4. 응급의료기관 위치정보 데이터 클래스 정의 ]
    데이터 클래스가 허술함의 극치를 달리고 있는 것을 확인하고 재작성하였음

--- src/main/kotlin/daelim/emergency_backend/models/EmergencyMedicalInstitutionLocation/EmergencyMedicalInstitutionLocationResult.kt
+++ src/main/kotlin/daelim/emergency_backend/models/EmergencyMedicalInstitutionLocation/EmergencyMedicalInstitutionLocationResult.kt
@@ -1,8 +1,94 @@
+
 package daelim.emergency_backend.models
 
-data class EmergencyMedicalInstitutionLocationResult(
-val resultCode:Int,//결과코드
-val resultMag : String, //결과메시지
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.annotation.JsonRootName
 
-val Items: List<EmergencyMedicalInstitutionLocation>
+
+//응급의료기관 위치정보 조회
+
+
+@JsonRootName("response")
+data class EmergencyMedicalInstitutionLocationResult(
+    @set:JsonProperty("header")
+    var header:Header?,
+
+    @set:JsonProperty("body")
+    var body:Body?,
+)
+
+
+@JsonRootName("header")
+data class Header(
+    @set:JsonProperty("resultCode")//결과코드
+    var resultCode:String?,
+
+    @set:JsonProperty("resultMsg")//결과메시지
+    var resultMsg:String?,
+)
+
+@JsonRootName("body")
+data class Body(
+    @set:JsonProperty("items")
+    var items:Items?,
+
+    @set:JsonProperty("numOfRows")//한 페이지 결과수
+    var numOfRows:Int?,
+
+    @set:JsonProperty("pageNo")//페이지번호
+    var pageNo:Int?,
+
+    @set:JsonProperty("totalCount")//총 결과수
+    var totalCount:Int?,
+)
+
+@JsonRootName("items")
+data class Items(
+    @set:JsonProperty("item")
+    var item:List<EmergencyMedicalInstitutionLocation>?,
+)
+
+@JsonRootName("item")
+data class EmergencyMedicalInstitutionLocation(
+    @set:JsonProperty("rnum")
+    var rnum: Int? = null, // 일련번호
+
+    @set:JsonProperty("cnt")
+    var cnt: Int? = null, // 건수
+
+    @set:JsonProperty("distance")
+    var distance: Double? = null, // 거리
+
+    @set:JsonProperty("dutyAddr")
+    var dutyAddr: String? = null, // 주소
+
+    @set:JsonProperty("dutyDiv")
+    var dutyDiv: String? = null, // 병원 분류
+
+    @set:JsonProperty("dutyDivName")
+    var dutyDivName: String? = null, // 병원 분류명
+
+    @set:JsonProperty("dutyFax")
+    var dutyFax: String? = null, // 팩스번호
+
+    @set:JsonProperty("dutyName")
+    var dutyName: String? = null, // 기관명
+
+    @set:JsonProperty("dutyTel1")
+    var dutyTel1: String? = null, // 대표전화1
+
+    @set:JsonProperty("endTime")
+    var endTime: String? = null, // 종료시간
+
+    @set:JsonProperty("hpid")
+    var hpid: String? = null, // 기관ID
+
+    @set:JsonProperty("latitude")
+    var latitude: Double? = null, // 병원위도
+
+    @set:JsonProperty("longitude")
+    var longitude: Double? = null, // 병원경도
+
+    @set:JsonProperty("startTime")
+    var startTime: String? = null // 시작시간
 )
