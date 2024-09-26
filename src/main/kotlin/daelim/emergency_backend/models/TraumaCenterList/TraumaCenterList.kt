e7783218b4beb53f9feaafa9880a5d7175dc1fac
huibeom <heebum9588@naver.com>
2024-09-26 23:44:51 +0900

    6. 외상센터 목록정보  데이터 클래스 정의
    Data Class 정의
    -아래는 지라 링크
    https://daelim-univ.atlassian.net/browse/PRO-275

---
+++ src/main/kotlin/daelim/emergency_backend/models/TraumaCenterList/TraumaCenterList.kt
@@ -0,0 +1,18 @@
+
+// data class TraumaCenterList(
+
+// val rnum:Int,//일련번호
+// val hpid:String,//기관 id
+// val phpid:String,//기관ID(OLD)
+// val dutyEmclsLString,//응급의료기관분류
+// val dutyEmclsName:String,//응급의료기관분류명
+// val dutyAddr:String,//주소
+// val dutyName:String ,//기관명
+
+
+// val dutyTel1:String,//대표전화1
+// val dutyTel3:String, //응급실전화
+
+// val wgs84Lon:String,//병원경도
+// val wgs84Lat:String,//병원위도
+// )
