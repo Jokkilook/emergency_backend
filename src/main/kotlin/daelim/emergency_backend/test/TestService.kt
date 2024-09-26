5140ee1b46301d4d52e96f8cc88fff445416172e
Jokkilook <tjddbs8295@gmail.com>
2024-09-26 22:15:02 +0900

    파라미터 인코딩 문제 해결

--- src/main/kotlin/daelim/emergency_backend/test/TestService.kt
+++ src/main/kotlin/daelim/emergency_backend/test/TestService.kt
@@ -7,11 +7,13 @@ import com.fasterxml.jackson.dataformat.xml.XmlMapper
 import com.fasterxml.jackson.dataformat.xml.ser.ToXmlGenerator
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import daelim.emergency_backend.models.AvailavleBedInfo.AvailableBedInfoResult
+import okio.ByteString.Companion.encode
 import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.util.UriComponentsBuilder
 import reactor.core.publisher.Mono
 import java.net.URI
+import java.net.URLEncoder
 
 @Service
 class TestService(val webClient: WebClient) {
@@ -20,17 +22,25 @@ class TestService(val webClient: WebClient) {
     //val serviceKey = "YXCUlt2omoo9wIHweuRa2AwH00oXWywq3Up/6DVims6C8XED7Xcyn4SR3WaU83G73CP3/upnkVWkJnbDvVa++g=="
     fun getTest(url:String):Mono<AvailableBedInfoResult>{
 
+        val stage1 = URLEncoder.encode("경기도", "UTF-8")
+        val stage2 = URLEncoder.encode("군포시", "UTF-8")
+
         val response = webClient.get()
             .uri{
                 it.path(url)
+                    .queryParam("STAGE1",stage1)
+                    .queryParam("STAGE2",stage2)
                 .queryParam("serviceKey",serviceKey)
                 .build()
             }
             .retrieve()
             .bodyToMono(String::class.java)
             .map { xmlString ->
-                print(xmlString)
+                println(xmlString)
                 convertXmlToAvailableBedInfoResult(xmlString) }
+        response.block()?.body?.items?.item?.forEach {
+            println(it.dutyName)
+        }
 
         return  response
     }
