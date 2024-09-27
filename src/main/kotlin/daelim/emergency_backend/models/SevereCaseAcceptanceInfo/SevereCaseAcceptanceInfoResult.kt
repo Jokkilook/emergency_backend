dfc5824dc3ea91992ed726fb69a31f66694fe509
Jokkilook <tjddbs8295@gmail.com>
2024-09-27 15:54:52 +0900

    2. 중증질환자 수용 가능 정보 변환 완료

--- src/main/kotlin/daelim/emergency_backend/models/SevereCaseAcceptanceInfo/SevereCaseAcceptanceInfoResult.kt
+++ src/main/kotlin/daelim/emergency_backend/models/SevereCaseAcceptanceInfo/SevereCaseAcceptanceInfoResult.kt
@@ -1,7 +1,74 @@
 package daelim.emergency_backend.models.SevereCaseAcceptanceInfo
 
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.annotation.JsonRootName
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper
+import daelim.emergency_backend.models.AvailavleBedInfo.AvailableBedInfoItems
+import daelim.emergency_backend.models.AvailavleBedInfo.Header
+
+@JsonRootName("response")
 data class SevereCaseAcceptanceInfoResult(
-    val resultCode:Int, //결과코드
-    val resultMsg:String, //결과메세지
-    val items:List<SevereCaseAcceptanceInfo> //중증질환자 수용가능 정보
+    @JsonProperty("header")
+    var header:Header?,
+    @JsonProperty("body")
+    var body:SevereCaseAcceptanceInfoBody?
+
+)
+
+@JsonRootName("body")
+data class SevereCaseAcceptanceInfoBody(
+    @JacksonXmlElementWrapper(useWrapping = false, localName = "items")
+    @set:JsonProperty("items")
+    var items: SevereCaseAcceptanceInfoItems?,
+    @set:JsonProperty("numOfRows")
+    var numOfRows:String?,
+    @set:JsonProperty("pageNo")
+    var pageNo:String?,
+    @set:JsonProperty("totalCount")
+    var totalCount:String?
+)
+
+@JsonRootName("items")
+data class SevereCaseAcceptanceInfoItems(
+    @JsonProperty("item")
+    var items:List<SevereCaseAcceptanceInfo>?
+)
+
+@JsonRootName("item")
+data class SevereCaseAcceptanceInfo(
+    var dutyName:String?, //기관명
+    var hpid:String?,//기관ID
+    var mkioskty28:String?, //응급실 가용 여부
+    var mkioskty1:String?, //재관류중재술 심근경색 가용 여부
+    var mkioskty2:String?, //재관류중재술 뇌경색 가용 여부
+    var mkioskty3:String?, //뇌출혈수술 거미막하출혈 가용 여부
+    var mkioskty4:String?, //뇌출혈수술 거미막하출혈 외 가용 여부
+    var mkioskty5:String?, //대동맥응급 흉부 가용 여부
+    var mkioskty6:String?, //대동맥응급 복부 가용 여부
+    var mkioskty7:String?, //담낭담관질환 담낭질환 가용 여부
+    var mkioskty8:String?, //담낭담관질환 담도포함질환 가용 여부
+    var mkioskty9:String?, //복부응급수술 비외상 가용 여부
+    var mkioskty10:String?, //장중첩/폐색 영유아 가용 여부
+    var mkioskty11:String?, //응급내시경 성인 위장관 가용 여부
+    var mkioskty12:String?, //응급내시경 영유아 위장관 가용 여부
+    var mkioskty13:String?, //응급내시경 성인 기관지 가용 여부
+    var mkioskty14:String?, //응급내시경 영유아 기관지 가용 여부
+    var mkioskty15:String?, //저출생체중아 집중치료 가용 여부
+    var mkioskty16:String?, //산부인과응급 분만 가용 여부
+    var mkioskty17:String?, //산부인과응급 산과수술 가용 여부
+    var mkioskty18:String?, //산부인과응급 부인과수술 가용 여부
+    var mkioskty19:String?, //중증화상 전문치료 가용 여부
+    var mkioskty20:String?, //사지접합 수족지접합 가용 여부
+    var mkioskty21:String?, //사지접합 수족지접합 외 가용 여부
+    var mkioskty22:String?, //응급투석 HD 가용 여부
+    var mkioskty23:String?, //응급투석 CRRT 가용 여부
+    var mkioskty24:String?, //정신과적응급 폐쇄병동입원 가용 여부
+    var mkioskty25:String?, //안과적수술 응급 가용 여부
+    var mkioskty26:String?, //영상의학혈관중재 성인 가용 여부
+    var mkioskty27:String?, //영상의학혈관중애 영유아 가용 여부
+    var M_KOISK_TY10_MSG:String?, //장중첩/폐색(영유아) 가능연령
+    var M_KOISK_TY12_MSG:String?, //위장관 응급내시경(영유아) 가능연령
+    var M_KOISK_TY14_MSG:String?, //기관지 응급내시경(영유아) 가능연령
+    var M_KOISK_TY15_MSG:String?, //저출생 체중아 가능연령
+    var M_KOISK_TY27_MSG:String?, //영상의학 혈관 중재적 시술(영유아) 가능연령
 )
