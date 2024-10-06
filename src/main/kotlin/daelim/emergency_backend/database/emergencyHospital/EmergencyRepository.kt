e94024aae600c65a2ca0c19f161472ab28982f56
wonwouk <Gmetodol1127@gmail.com>
2024-10-07 00:27:41 +0900

    refactor(Repository) : String를 Long으로 변경

--- src/main/kotlin/daelim/emergency_backend/database/emergencyHospital/EmergencyRepository.kt
+++ src/main/kotlin/daelim/emergency_backend/database/emergencyHospital/EmergencyRepository.kt
@@ -2,4 +2,4 @@ package daelim.emergency_backend.database.emergencyHospital
 
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface EmergencyRepository:JpaRepository<EmergencyHospitalData, String> {}
+interface EmergencyRepository:JpaRepository<EmergencyHospitalData, Long> {}
