00431f257233195a0b97ce5be2d8a343db273498
wonwouk <Gmetodol1127@gmail.com>
2024-10-07 00:32:06 +0900

    update(Repository): 조인 쿼리문 추가
    - HospitalInformation과 EmergencyHospitalData간의 hpid를 통한 단방향 조인 추가

--- src/main/kotlin/daelim/emergency_backend/database/hospitalInformation/HospitalRepository.kt
+++ src/main/kotlin/daelim/emergency_backend/database/hospitalInformation/HospitalRepository.kt
@@ -1,5 +1,14 @@
 package daelim.emergency_backend.database.hospitalInformation
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.stereotype.Repository
 
-interface HospitalRepository:JpaRepository<HospitalInformation, String> {}
+@Repository
+interface HospitalRepository : JpaRepository<HospitalInformation, Long> {
+    @Query("""SELECT hi, eh
+              FROM HospitalInformation hi 
+              JOIN EmergencyHospitalData eh ON hi.hpid = eh.hpid 
+              WHERE hi.hpid = :hpid""", nativeQuery = false)//hi(병원)의 hpid와 eh(응급실)의 hpid로 관계형성하여 조인
+    fun findByHpid(hpid: String): List<HospitalInformation>
+}
