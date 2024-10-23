e0cf02a75b94167b80d77f6990ab3c4ce8a3de93
Jokkilook <tjddbs8295@gmail.com>
2024-10-23 12:02:13 +0900

    API 코드 누락 수정

--- src/main/kotlin/daelim/emergency_backend/test/TestController.kt
+++ src/main/kotlin/daelim/emergency_backend/test/TestController.kt
@@ -3,21 +3,11 @@ package daelim.emergency_backend.test
 import daelim.emergency_backend.database.emergencyHospital.EmergencyHospitalData
 import daelim.emergency_backend.database.EmergencyService
 import daelim.emergency_backend.database.hospitalInformation.HospitalInformation
-import daelim.emergency_backend.models.AvailavleBedInfo.AvailableBedInfoResult
-import daelim.emergency_backend.models.EmergencyAndSevereCaseMessage.EmergencyAndSevereCaseMessageResult
-import daelim.emergency_backend.models.EmergencyMedicalInstitutionBasicInfoResult
-import daelim.emergency_backend.models.EmergencyMedicalInstitutionInfo.EmergencyMedicalInstitutionInfoResult
-import daelim.emergency_backend.models.EmergencyMedicalInstitutionLocationResult
 import daelim.emergency_backend.models.Response
-import daelim.emergency_backend.models.SevereCaseAcceptanceInfo.SevereCaseAcceptanceInfoResult
-import daelim.emergency_backend.models.TraumaCenterBasicInfo.TraumaCenterBasicInfoResult
-import daelim.emergency_backend.models.TraumaCenterListResult
-import daelim.emergency_backend.models.TraumaCenterLocation.TraumaCenterLocationResult
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.data.domain.Page
 import org.springframework.http.HttpStatus
-import org.springframework.http.HttpStatusCode
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -59,6 +49,21 @@ class TestController(val testService: TestService, val emergencyService: Emergen
         } catch (e:Exception) {
             ResponseEntity(Response(HttpStatus.INTERNAL_SERVER_ERROR.value(),e.message.toString(),null),null, HttpStatus.INTERNAL_SERVER_ERROR)
         }
+    }
+
+    //hospital information List 반환
+    @Operation(summary = "병원 정보 리스트 반환", description = "병원 데이터를 페이징하여 반환합니다.")
+    @GetMapping("/getHospitalInfoList")
+    fun getHospitalList(
+        @RequestParam(defaultValue = "0") page: Int,
+        @RequestParam(defaultValue = "20") size: Int
+    ): ResponseEntity<Response<Page<HospitalInformation>>?> {
+        return try {
+            ResponseEntity(Response(HttpStatus.OK.value(),"success",emergencyService.getHospitalInformationsByPage(page, size)),null,HttpStatus.OK)
+        } catch (e:Exception) {
+            ResponseEntity(Response(HttpStatus.INTERNAL_SERVER_ERROR.value(),e.message.toString(),null),null, HttpStatus.INTERNAL_SERVER_ERROR)
+        }
+
     }
 
     @Operation(summary = "병원 정보와 응급실 정보 반환", description = "hpid로 병원 정보와 응급실 정보를 선택적으로 반환합니다.")
