b87c3a578aac7ccf8fcc3479c39ce5ce84d5c512
Jokkilook <tjddbs8295@gmail.com>
2024-11-06 11:33:12 +0900

    주석 추가

--- src/main/kotlin/utils/EmergencyUtils.kt
+++ src/main/kotlin/utils/EmergencyUtils.kt
@@ -4,16 +4,18 @@ import kotlin.math.*
 
 class EmergencyUtils {
     companion object {
+        //m단위 변한
         fun getDistanceWithLonLat(originLat:Double, originLon:Double, targetLat:Double, targetLon:Double):Double{
             var distance = 0.0
             val R = 6371.0 //지구 평균 반지름 (km)
-
+            
             val dLat = Math.toRadians(targetLat - originLat)
             val dLon = Math.toRadians(targetLon - originLon)
 
             val a = sin(dLat/2).pow(2) + cos(Math.toRadians(originLat)) * cos(Math.toRadians(targetLat)) * sin(dLon/2).pow(2)
             val c = 2 * atan2(sqrt(a), sqrt(1-a))
-
+            
+            //1000을 곱해서 m단위로 변경
             distance = R * c * 1000
 
             return distance
