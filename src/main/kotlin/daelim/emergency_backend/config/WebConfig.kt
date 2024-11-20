189555400e44da4850363e0664ad3bcbbaeb89ac
Jokkilook <tjddbs8295@gmail.com>
2024-11-20 22:17:20 +0900

    오류 수정

--- src/main/kotlin/daelim/emergency_backend/config/WebConfig.kt
+++ src/main/kotlin/daelim/emergency_backend/config/WebConfig.kt
@@ -8,14 +8,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
 class WebConfig:WebMvcConfigurer {
-    override fun extendMessageConverters(converters: MutableList<HttpMessageConverter<*>>) {
-        // XML 메시지 컨버터 제거
-        converters.removeIf { it.javaClass.name.contains("MappingJackson2XmlHttpMessageConverter") }
-    }
-
     override fun configureMessageConverters(converters: MutableList<HttpMessageConverter<*>>) {
         // JSON 메시지 컨버터 우선순위로 추가
-        converters.add(0, MappingJackson2HttpMessageConverter())
+        converters.clear()
+        converters.add(MappingJackson2HttpMessageConverter())
     }
 
     override fun configureContentNegotiation(configurer: ContentNegotiationConfigurer) {
