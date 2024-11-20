05ae85b14c9cf684e7a3e90e5f4053cad1144240
huibeom <heebum9588@naver.com>
2024-11-21 03:48:17 +0900

    cors error 해결 위한 security 추가

---
+++ src/main/kotlin/daelim/emergency_backend/config/SecurityConfig.kt
@@ -0,0 +1,53 @@
+package daelim.emergency_backend.config
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig() {
+    @Bean
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        return http
+            .formLogin { it.disable() }
+            .httpBasic { it.disable() }
+            .csrf { it.disable() }
+            .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
+            .cors { it.configurationSource(corsConfigurationSource()) } // CORS 관련 설정
+            .authorizeHttpRequests {
+                it
+                    .requestMatchers("/v1/app/**")//여기에 여러개 넣음
+                    .anonymous()
+//                    .requestMatchers("/api/member/**").hasRole("MEMBER")
+                    .anyRequest().permitAll()
+            }
+            .build()
+    }
+
+    @Value("\${OUTBOUND_URL}")
+    lateinit var outboundUrl: String
+
+    // CORS 관련 설정
+    @Bean
+    fun corsConfigurationSource(): CorsConfigurationSource {
+        val config = CorsConfiguration()
+        config.allowedOrigins = listOf("https://prod-1066673057371.asia-northeast3.run.app") // frontend url
+        config.allowedMethods = listOf("GET")
+//        config.allowedHeaders = listOf("*")
+        config.allowedHeaders = mutableListOf("Content-Type", "Authorization")
+        config.allowCredentials = true
+        config.maxAge = 3600L
+
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("v1/app/**", config)
+        return source
+    }
+}
