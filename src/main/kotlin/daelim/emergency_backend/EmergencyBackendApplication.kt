fa2afe218e577d9f08bae051abfa8d6e71c7cfa7
huibeom <151634498+melonNamedLove@users.noreply.github.com>
2024-11-22 19:36:37 +0900

    EmergencyBackendApplication.kt 업데이트

--- src/main/kotlin/daelim/emergency_backend/EmergencyBackendApplication.kt
+++ src/main/kotlin/daelim/emergency_backend/EmergencyBackendApplication.kt
@@ -2,6 +2,14 @@ package daelim.emergency_backend
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.servers.Server
+
+@OpenAPIDefinition(
+    servers = [
+        Server(url = "/", description = "Default Server URL")
+    ]
+)
 
 @SpringBootApplication
 class EmergencyBackendApplication
